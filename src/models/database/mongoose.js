53dd4a7d05e3a231cb71f20412ed8fd67c0b8b9d
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-11-19 18:01:36 +0700

    [config] change mongodb server

--- src/models/database/mongoose.js
+++ src/models/database/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-const CONNECTION_URL = 'mongodb://172.17.0.2:27017/user-service';
+// const CONNECTION_URL = 'mongodb://172.17.0.2:27017/user-service';
+const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:33303';
 mongoose.connect(CONNECTION_URL, {
   useNewUrlParser: true,
+  user: "root",
+  pass: "123456",
+  dbName: "user-service"
 });
