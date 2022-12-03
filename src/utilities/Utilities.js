0911c652bbcfd432472064ef739f4a1585d7d2fc
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-12-03 17:03:06 +0700

    [utility] add sendEmail feature

--- src/utilities/Utilities.js
+++ src/utilities/Utilities.js
@@ -1,4 +1,7 @@
 const slugify = require('slugify');
+const nodemailer = require('nodemailer');
+const { User } = require('../models/_User');
+
 
 const slug = function (str, more) {
   return slugify(str, {
@@ -9,6 +12,40 @@ const slug = function (str, more) {
   })
 }
 
+const sendEmail = async (email) => {
+  const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      service: "gmail",
+      port: 587,
+      secure: true,
+      auth: {
+          user: "thanhduong10022001@gmail.com",
+          pass: "fdoksxyjumlyhtla",
+      }
+  });
+  //
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error(`Not found user with email [${email}]`);
+    }
+    //
+    const userId = lodash.get(user, "_id");
+    const UI_URL = "";
+    await transporter.sendMail({
+        from: "thanhduong10022001@gmail.com",
+        to: "trunghieuvan01@gmail.com",
+        subject: "Reset password link",
+        text: `Your reset password link is: ${UI_URL}/${userId}`,
+    });
+
+    return { message: "email was sent successfully" };
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
-  slug
+  slug,
+  sendEmail
 }
