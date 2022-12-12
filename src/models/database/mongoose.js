9379a07e91bd156542c5606a164c729a55490fd6
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-12-12 13:41:31 +0700

    [mongo] change port

--- src/models/database/mongoose.js
+++ src/models/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const CONNECTION_URL = 'mongodb://172.17.0.2:27017/user-service';
-const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:41050';
+const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:60390';
 mongoose.connect(CONNECTION_URL, {
   useNewUrlParser: true,
   user: "root",
